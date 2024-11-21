d5c7b020772ccc8230aeb9b64c3bc9835fb1b4dc
hamster0410 <gkstjsdn0011@naver.com>
2024-11-21 16:40:09 +0900

    hsw

---
+++ week7/1541/1541_hsw.java
@@ -0,0 +1,21 @@
+int sum = Integer.MAX_VALUE;	// 초기 상태 여부 확인을 위한 값으로 설정 
+StringTokenizer subtraction = new StringTokenizer(br.readLine(), "-");
+ 
+while (subtraction.hasMoreTokens()) {
+	int temp = 0;
+ 
+	// 뺄셈으로 나뉜 토큰을 덧셈으로 분리하여 해당 토큰들을 더한다.
+	StringTokenizer addition = new StringTokenizer(subtraction.nextToken(), "+");
+			
+	// 덧셈으로 나뉜 토큰들을 모두 더한다. 
+	while (addition.hasMoreTokens()) {
+		temp += Integer.parseInt(addition.nextToken());
+	}
+			
+	// 첫 번째토큰인 경우 temp값이 첫 번째 수가 됨
+	if (sum == Integer.MAX_VALUE) {
+		sum = temp;
+	} else {
+		sum -= temp;
+	}
+}
